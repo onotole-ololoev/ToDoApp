4e7040685cae9498d3dd8636b495a69907383c62
onotole-ololoev <tolya.tar@gmail.com>
2021-08-04 17:53:20 +0300

    header component created

---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './header.css';
+
+const Header = () => {
+    return (
+        <>
+            <h1>My todo List</h1>
+            <div>
+
+            </div>
+        </>
+    )
+}
+
+export default Header;
